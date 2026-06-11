2201906bb9edd3ec17d5e559c997c979347f6ffc
Vladislav Slobodchikov <vslobodchikov@vvo.local>
2026-06-11 22:49:51 +1000

    Refactor code

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-num_sum = 0
-for num in range(1, 101):
-    if num % 2 == 0:
-        num_sum += num
+def get_sum_even_numbers(start: int, end: int) -> int:
+    num_sum = 0
+    for num in range(start, end + 1):
+        if num % 2 == 0:
+            num_sum += num
 
-squares = [num ** 2 for num in range(1, 11) if num % 2 != 0]
+    return num_sum
 
-print(f"Сумма всех нечетных числе от 1 до 100: {num_sum}")
+def get_squares(start: int, end: int) -> list:
+    return [num ** 2 for num in range(start, end + 1) if num % 2 != 0]
+
+def get_numbers_count():
+    nums_count = 0
+    input_num = 0
+    while input_num >= 0:
+        try:
+            input_num = float(input("Введите число: "))
+        except ValueError:
+            print("Введено не число.")
+            continue
+        nums_count += 1
+
+    return nums_count
+
+
+num_sum = get_sum_even_numbers(1, 101)
+print(f"Сумма всех нечетных чисел от 1 до 100: {num_sum}")
+
+squares = get_squares(1, 11)
 print(f"Список квадратов нечетных чисел от 1 до 10 - {squares}")
 
-nums_count = 0
-input_num = 0
-while input_num >= 0:
-    try:
-        input_num = float(input("Введите число: "))
-    except ValueError:
-        print("Введено не число.")
-        continue
-    nums_count += 1
-
+nums_count = get_numbers_count()
 print(f"Количество введенных чисел - {nums_count}")
 print("Введено отрицательное число. Завершение работы программы.")
